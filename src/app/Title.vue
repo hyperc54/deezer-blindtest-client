a6129b24acc4d23dfd0525b5aa92fb5c741ea336
hyperc54 <pthary54@gmail.com>
2017-01-30 19:24:32 +0100

    add socket io package dep

--- src/app/Title.vue
+++ src/app/Title.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="title">
     <h1 class="title-h1">Welcome in Deezer own BlindTest app !</h1>
-    <h2 class="title-h2">CONNEXION</h2>
+    <h2 class="title-h2"><a href="/public">Join a room NOW</a></h2>
   </div>
 </template>
 
--- src/app/game/Player.vue
+++ src/app/game/Player.vue
@@ -19,13 +19,12 @@
     mounted(){
       DZ.init({
         appId  : '130151',
-        channelUrl : 'http://localhost:3000',
+        channelUrl : 'http://localhost:3002',
         player: {
           container: 'dz-root',
           width:0,
           height:0,
           onload:function(){
-            console.log('!!!!!');
             DZ.getLoginStatus(function(response) {
               console.log(response);
               if (response.authResponse) {
@@ -38,47 +37,6 @@
           }
         }
       });
-
-
-      console.log('mounted');
-      /**
-      window.dzAsyncInit = function() {
-        DZ.init({
-          appId  : '130151',
-          channelUrl : 'http://localhost:3000',
-          player: {
-            container: 'dz-root',
-            width:0,
-            height:0,
-            onload:function(){
-              console.log('!!!!!');
-              DZ.getLoginStatus(function(response) {
-                console.log(response);
-                if (response.authResponse) {
-                  console.log('user logged in');
-                  // logged in and connected user, someone you know
-                } else {
-                  // no user session available, someone you dont know
-                }
-              });
-            }
-          }
-        });
-        console.log('!');
-
-
-        DZ.ready(function(sdk_options){
-
-        });
-
-      };
-      (function() {
-        var e = document.createElement('script');
-        e.src = 'https://e-cdns-files.dzcdn.net/js/min/dz.js';
-        e.async = true;
-        document.getElementById('dz-root').appendChild(e);
-      }());
-       **/
     }
 
   };
